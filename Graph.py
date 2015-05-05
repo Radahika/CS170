21e88b0acf100104e9a31e7cb818abe23ecb530b
Kyle Chun <kbchun@berkeley.edu>
2015-05-04 22:11:41 -0700

    added beginning stages of code for Phase 2

---
+++ Graph.py
@@ -0,0 +1,45 @@
+class Graph(object):
+
+    def __init__(self, matrix_graph, order_colors = None):
+        self.matrix_graph = matrix_graph
+        self.quantity_nodes = len(matrix_graph)
+        self.color_of_nodes = {}
+        
+        if order_colors:
+            for x in range(len(order_colors)):
+                if order_colors[x] == 'B':
+                    self.color_of_nodes[x+1] = 'blue'
+                else:
+                    self.color_of_nodes[x+1] = 'red'
+                
+        else:
+            for node in range(1, self.quantity_nodes+1):
+                self.color_of_nodes[node] = None
+
+
+    def assign_colors(self, index_node, c):
+        self.color_of_nodes[index_node] = c
+
+
+    def find_neighbors(self, node):
+        neighbors = {}
+        for x in range(1, self.quantity_nodes+1):
+            if x != node:
+                neighbors[x] = self.cost_edge(node, x)
+        return neighbors
+
+
+    def color_of_vertex(self, index_node):
+        return self.color_of_nodes[index_node]
+
+
+    def cost_edge(self, endpoint1, endpoint2):
+        return self.adj_matrix[endpoint1-1][endpoint2-1]
+
+
+    def cost_path(self, p):
+        return
+
+
+    def quantity_nodes(self):
+        return self.quantity_nodes
---
+++ solver.py
@@ -0,0 +1,36 @@
+from Graph import Graph
+import sys
+
+def path_finder(g):
+    """return NPTSP path with lowest path"""
+    return
+
+def legitimate_path(g, p):
+    """checks validity of path"""
+    return
+
+def brute_force(g):
+    """go through all permutations of possible paths if graph is small enough"""
+    return
+
+def output(input_quantity):
+    solutions = []
+    for x in range(1, input_quantity + 1):
+        solutions.append(read_input(x))
+    with open('answer.txt', 'w') as answer:
+        for p in solutions:
+            answer.write('%s\n'%' '.join(map(str, p)))
+
+def read_input(index_file):
+    title = 'input_files/{0}.in'.format(index_file)
+    with open(title) as data:
+        nodes = int(data.readline())
+        matrix_graph = [[] for i in range(nodes)]
+        for i in range(nodes):
+            matrix_graph[i] = [int(k) for k in data.readline().split()]
+        order_colors = data.readline()
+    g = Graph(matrix_graph, order_colors)
+    return path_finder(g)
+
+# if __name__ == '__main__':
+#     output(int(sys.argv[1]))
